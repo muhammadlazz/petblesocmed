7753466f68f815f36eea4d25a78ad2f4a3e814a4
muhammadlazz <muhlazuuardi@gmail.com>
2025-03-11 19:41:48 +0700

    update notification

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-do
 import { FiMoon, FiSun, FiInstagram, FiYoutube, FiTwitter, FiFacebook } from "react-icons/fi";
 import "./index.css";
 import "./App.css";
-// import AboutUs from "./components/AboutUs";
 import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import SignIn from "./components/SignIn";
 import Discovery from "./components/Discovery";
 // import Mail from "./components/Mail";
-// import Notification from "./components/Notification";
+import Notification from "./components/Notification";
 // import Settings from "./components/Settings";
 
 const App: React.FC = () => {
@@ -74,9 +73,9 @@ const App: React.FC = () => {
             <Route path="/about" element={<AboutUs />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/discovery" element={<Discovery />} />
-            {/* <Route path="/mail" element={<Mail />} />
+            {/* <Route path="/mail" element={<Mail />} /> */}
             <Route path="/notifications" element={<Notification />} />
-            <Route path="/settings" element={<Settings />} /> */}
+            {/* <Route path="/settings" element={<Settings />} /> */}
           </Routes>
         </main>
 
